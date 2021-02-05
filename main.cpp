4d1162d8c1f4daeb88630fc731ba75023fe19d7e
regelo <34382451+regelo@users.noreply.github.com>
2021-02-05 11:19:45 -0500

    Update main.cpp
    Ajout du mot "tout"

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Salut le monde !";
+    std::cout << "Salut tout le monde !";
     int x = 12;
     std::cout << x << std::endl;
     std::cout << "salut" << std::endl;
