35cc77c9693273ae5d75797ea95b7596447c428d
regelo <regelo@regelo.org>
2021-02-08 08:44:39 -0500

    Changement de x pour mois et initialisation à 12

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(){
     std::cout << "Hola tout le monde !";
-    int x = 38;
-    std::cout << x << std::endl;
+    int mois = 12;
+    std::cout << mois << std::endl;
     std::cout << "salut" << std::endl;
 }
